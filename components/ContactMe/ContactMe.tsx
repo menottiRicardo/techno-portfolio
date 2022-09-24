77ae669db2518901565345d1eb8b1442726a5b27
Ricardo Menotti <86802680+menottiRicardo@users.noreply.github.com>
2022-09-23 22:13:06 -0500

    contact :+1:

--- components/ContactMe/ContactMe.tsx
+++ components/ContactMe/ContactMe.tsx
@@ -15,7 +15,7 @@ const ContactMe = () => {
           Contact
         </h3>
 
-        <div>
+        <div className="space-y-10">
           <h4 className="text-4xl font-semibold text-center">
             We got just what you need.
           </h4>
@@ -23,21 +23,32 @@ const ContactMe = () => {
           <span className="decoration-[#F7AB0A]/50 underline">Lets Talk.</span>
           <div>
             <div className="flex items-center space-x-5 justify-center">
-              <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse"/>
+              <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
               <p className="text-2xl">+507124243</p>
             </div>
 
             <div className="flex items-center space-x-5 justify-center">
-              <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse"/>
+              <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
               <p className="text-2xl">Panama, Panama</p>
             </div>
 
             <div className="flex items-center space-x-5 justify-center">
-              <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse"/>
+              <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
               <p className="text-2xl">Panama, Panama</p>
             </div>
           </div>
         </div>
+
+        <form className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input type="text" className="contactInput"/>
+            <input type="text" className="contactInput"/>
+          </div>
+          <input type="text" className="contactInput"/>
+
+          <textarea className="contactInput"/>
+          <button className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold">Submit</button>
+        </form>
       </motion.div>
     </section>
   );
