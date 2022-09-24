67d863cb30e92a9d4c0fff792b173f2a7775575e
Ricardo Menotti <86802680+menottiRicardo@users.noreply.github.com>
2022-09-23 19:42:54 -0500

    hero done :100:

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 const Hero = () => {
   const [text, count] = useTypewriter({
     words: ["Experiences", "Solutions", "Apps"],
@@ -10,15 +12,45 @@ const Hero = () => {
   });
   return (
     <section id="hero" className="snap-center">
-      <motion.div className="h-screen flex flex-col space-y-0 items-center justify-center text-center overflow-hidden">
+      <div className="h-screen flex flex-col space-y-0 items-center justify-center text-center overflow-hidden">
         <BackgroundCircles />
 
-        <h1>
-          <span>We Develop </span>
-          <span>{text}</span>
-          <Cursor cursorColor="#F7AB0A" />
-        </h1>
-      </motion.div>
+        <div className="relative h-32 w-32">
+          <Image
+            className="relative h-32 w-32 mx-auto"
+            src="/logo.png"
+            alt="logo"
+            layout="fill"
+          />
+        </div>
+
+        <div className="z-20">
+          <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+            Menotti Solutions
+          </h2>
+          <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+            <span>We Develop </span>
+            <span>{text}</span>
+
+            <Cursor cursorColor="#F7AB0A" />
+          </h1>
+
+          <div className="pt-5">
+            <Link href={"#about"}>
+              <button className="heroButton">About</button>
+            </Link>
+            <Link href="#experience">
+              <button className="heroButton">Experience</button>
+            </Link>
+            <Link href={"#skills"}>
+              <button className="heroButton">Skills</button>
+            </Link>
+            <Link href={"#projects"}>
+              <button className="heroButton">Projects</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
