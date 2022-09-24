9e8169c58d5a6e5200129df9dd0d7e6d97d4496a
Ricardo Menotti <86802680+menottiRicardo@users.noreply.github.com>
2022-09-23 21:45:48 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import About from "../components/About";
+
 import Experience from "../components/Experience";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
@@ -19,8 +20,8 @@ const Home: NextPage = () => {
 
       
       <Hero />
-     
-      <About />
+     <About />
+      
 
       <Experience />
 
