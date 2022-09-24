1a1037fdfac7b5c143fa0269ddf4ae6065997160
Ricardo Menotti <86802680+menottiRicardo@users.noreply.github.com>
2022-09-23 23:08:47 -0500

    send to contact

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { EnvelopeIcon } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
+import Link from "next/link";
 const Header = () => {
   return (
     <header className="sticky top-0 flex items-start justify-end max-w-7xl p-5 z-20 ">
-      <motion.div
-        className="flex flex-row items-center text-gray-300 cursor-pointer"
-        initial={{ x: 500, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{duration: 1.5}}
-      >
-        <EnvelopeIcon className="w-5 mr-2" />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-          Get In Touch
-        </p>
-      </motion.div>
+      <Link href={"#contact"}>
+        <motion.div
+          className="flex flex-row items-center text-gray-300 cursor-pointer"
+          initial={{ x: 500, opacity: 0, scale: 0.5 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1.5 }}
+        >
+          <EnvelopeIcon className="w-5 mr-2" />
+
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            Get In Touch
+          </p>
+        </motion.div>
+      </Link>
     </header>
   );
 };
